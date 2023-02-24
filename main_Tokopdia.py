204b49e8d0c0686868886d8ea0a70bd28c645de2
xzoom117 <amanwhohave@gmail.com>
2023-02-24 14:47:50 +0700

    Add json

---
+++ main_Tokopdia.py
@@ -0,0 +1,55 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+maxPage = 6
+
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+}
+
+result = []
+id = 1
+
+for page_num in range(1, maxPage):  # ambil data dari 5 halaman awal
+    URL = f"https://www.tokopedia.com/p/komputer-laptop/laptop/ultrabook?page={page_num}"
+    page = requests.get(URL, headers=headers)  # mengunduh halaman
+    soup = BeautifulSoup(page.content, "html.parser")  # ekstraksi kode html
+    tokopedia = soup.find(class_="css-13l3l78 e1nlzfl9")  # mengambil produk
+
+    # BAGIAN ISI
+    image = tokopedia.find_all("img")
+    title = tokopedia.find_all("span", class_="css-1bjwylw")
+    discount = tokopedia.find_all("div", class_="css-rn1hus")
+    priceBeforeDisc = tokopedia.find_all("div", class_="css-rn1hus")
+    priceAfterDisc = tokopedia.find_all("span", class_="css-o5uqvq")
+    infoToko = tokopedia.find_all("span", class_="css-1kr22w3")
+
+    # BAGIAN ISI
+    j = 0
+    for i in range(len(title)):
+        tempDiscount = "0%"
+        tempPrice = priceAfterDisc[i].text.strip()
+        try:
+            tempDiscount = discount[i].text.strip().split("%")[0]
+            tempPrice = discount[i].text.strip().split("%")[1]
+        except IndexError:
+            tempDiscount = "0"
+
+        result.append(
+            {"id": id,
+             "gambar": image[i].attrs['src'],
+             "produk": title[i].text.strip(),
+             "diskon": tempDiscount+"%",
+             "harga sebelum diskon": tempPrice,
+             "harga setelah diskon": priceAfterDisc[i].text.strip(),
+             "tempat toko": infoToko[j].text.strip(),
+             "nama toko": infoToko[j+1].text.strip()
+             }
+        )
+        j += 2
+        id += 1
+
+HasilJSON = json.dumps(result, indent=4)
+JSONFile = open("ultrabook.json", "w")
+JSONFile.write(HasilJSON)
+JSONFile.close()
---
+++ ryy.py
@@ -0,0 +1,58 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+maxPage = 6
+
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+}
+
+result = []
+id = 1
+
+for page_num in range(1, maxPage):  # ambil data dari 5 halaman awal
+    URL = f"https://www.tokopedia.com/p/komputer-laptop/laptop/ultrabook?page={page_num}"
+    page = requests.get(URL, headers=headers)  # mengunduh halaman
+    soup = BeautifulSoup(page.content, "html.parser")  # ekstraksi kode html
+    tokopedia = soup.find(class_="css-13l3l78 e1nlzfl9")  # mengambil produk
+
+    # foto = soup.find(class_="css-1g5og91")
+    # BAGIAN ISI
+
+    # Finding all the images in the page.
+    image = tokopedia.find_all("div", class_="css-1g5og91")
+    title = tokopedia.find_all("span", class_="css-1bjwylw")
+    discount = tokopedia.find_all("div", class_="css-rn1hus")
+    priceBeforeDisc = tokopedia.find_all("div", class_="css-rn1hus")
+    priceAfterDisc = tokopedia.find_all("span", class_="css-o5uqvq")
+    infoToko = tokopedia.find_all("span", class_="css-1kr22w3")
+    print(len(image))
+    # BAGIAN ISI
+    j = 0
+    for i in range(len(title)):
+        tempDiscount = "0%"
+        tempPrice = priceAfterDisc[i].text.strip()
+        try:
+            tempDiscount = discount[i].text.strip().split("%")[0]
+            tempPrice = discount[i].text.strip().split("%")[1]
+        except IndexError:
+            tempDiscount = "0"
+
+        result.append(
+            {"id": id,
+             #  "gambar": image[i]['src'],
+             "produk": title[i].text.strip(),
+             "diskon": tempDiscount+"%",
+             "hargasebelumdiskon": tempPrice,
+             "hargasetelahdiskon": priceAfterDisc[i].text.strip(),
+             "tempattoko": infoToko[j].text.strip(),
+             "namatoko": infoToko[j+1].text.strip()
+             }
+        )
+        j += 2
+        id += 1
+
+HasilJSON = json.dumps(result, indent=4)
+JSONFile = open("ultrabook.json", "w")
+JSONFile.write(HasilJSON)
+JSONFile.close()
